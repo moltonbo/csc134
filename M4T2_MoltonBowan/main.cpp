32707561f2ba378e68cf9885e411682637c767f3
moltonbo <125904023+moltonbo@users.noreply.github.com>
2023-02-27 11:12:22 -0500

    create project

---
+++ M4T2_MoltonBowan/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// CSC 134
+// M4T2 - Menus
+// Oriah Molton Bowman
+// 27 Feb 2023
+
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
