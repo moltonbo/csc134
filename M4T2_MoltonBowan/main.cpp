31f660643de70317e10d372a6d232a18c98540c8
moltonbo <125904023+moltonbo@users.noreply.github.com>
2023-02-27 11:43:23 -0500

    update part 1

--- M4T2_MoltonBowan/main.cpp
+++ M4T2_MoltonBowan/main.cpp
@@ -5,11 +5,55 @@ using namespace std;
 // Oriah Molton Bowman
 // 27 Feb 2023
 
+// menu function
+void option_one () {
+    // count from 0 to 9
+    cout << "Using while() to count"<< endl;
+    int num =0;
+    while (num < 10){
+     cout << num <<" " ;
+     num ++;
+
+}
+cout<< endl;
+cout << "Using for() to count"<< endl;
+for (int i=0; i<10; i++) {
+    cout << i <<" ";
+}
+cout << endl;
+}
+void option_two(){
+ // sum up five numbers
+}
+void option_three(){
+//ToDO
+}
+
 
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Welcome to the menu" << endl;
+    cout << "Press 1 to count "<< endl;
+    cout << "Press 2 to sum up numbers"<< endl;
+    cout << "press 3 TODO" << endl;
+    cout<< "Choice:";
+
+    // TODO input validation
+
+
+
+    int choice;
+    cin >> choice;
+    // user number
+    while ( choice < 1|| choice >3){
+        cout << "please choose 1,2,or 3: ";
+        cin >> choice;
+    }
+    // if we get here we know choice is valid
+    if(choice == 1){
+        option_one(); // function call
+    }
 
     return 0;
 }
